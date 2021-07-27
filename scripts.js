181694a8cfa249df5cc8b7f9dfdb5e17e2eb9e05
claudiahW <murungiclaud@gmail.com>
2021-07-27 23:49:48 +0300

    add js functions

--- scripts.js
+++ scripts.js
@@ -12,3 +12,19 @@ var Calculator = function() {
         .value;
         var dateOfBirth = new Date(year + "/" + month + "/" + date);
         var weekDay = dateOfBirth.getDay(); 
+
+        if (month < 1 || month > 12 || (month == 2 && date > 29)) {
+            return (document.getElementById("alert2").innerHTML =
+                "INPUT A VALID MONTH!");
+            } else if (date < 1 || date > 31) {
+            return (document.getElementById("alert2").innerHTML =
+                "INPUT A VALID DATE!");
+            } else if (year === NaN || year < 1900 || year > 2025) {
+            document.getElementById("alert2").innerHTML =
+                "INPUT A VALID YEAR!";
+            }
+            var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+            var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        
+            var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
