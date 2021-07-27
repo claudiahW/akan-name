6a284e684cfdf3364b0990ffb2970c342c79ee0a
claudiahW <murungiclaud@gmail.com>
2021-07-27 23:46:28 +0300

    add git element id

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var Calculator = function() {
+    var date = parseInt(
+    document.getElementById("Form").elements.namedItem("date").value
+    );
+    var month = parseInt(
+        document.getElementById("Form").elements.namedItem("month").value
+        );
+        var year = parseInt(
+        document.getElementById("Form").elements.namedItem("year").value
+        );
+        var gender = document.getElementById("Form").elements.namedItem("gender")
+        .value;
+        var dateOfBirth = new Date(year + "/" + month + "/" + date);
+        var weekDay = dateOfBirth.getDay(); 
